6378eeb6ffd77500500d3d6c48ebe79f76bf8ecb
tfs-code <tylerfishersmith@gmail.com>
2026-04-21 11:46:39 +0100

    feat: implement save playlist functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -41,6 +41,7 @@ class App extends React.Component {
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
         this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlaylist = this.savePlaylist.bind(this);
     }
 
     addTrack(track) {
@@ -66,6 +67,10 @@ class App extends React.Component {
         this.setState({ playlistName: name });
     }
 
+    savePlaylist() {
+        const trackURIs = this.state.playlistTracks.map((track) => track.uri);
+    }
+
     render() {
         return (
             <div>
@@ -87,6 +92,7 @@ class App extends React.Component {
                             playlistTracks={this.state.playlistTracks}
                             onRemove={this.removeTrack}
                             onNameChange={this.updatePlaylistName}
+                            onSave={this.savePlaylist}
                         />
                     </div>
                 </div>
